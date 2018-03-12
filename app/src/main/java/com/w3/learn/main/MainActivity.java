84b4539a5439e2d95be5588cbe0a5ed36f06d00f
nixonok <nixonok@nixonok.xyz>
2018-03-12 19:27:12 +0600

    Added image loader to background thread and reader to presenter

--- app/src/main/java/com/w3/learn/main/MainActivity.java
+++ app/src/main/java/com/w3/learn/main/MainActivity.java
@@ -2,69 +2,40 @@ package com.w3.learn.main;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.w3.learn.R;
 import com.w3.learn.base.BaseActivity;
+import com.w3.learn.main.MainMvpView;
+import com.w3.learn.main.MainPresenter;
+import com.w3.learn.main.ShowImageFullScreen;
 
 import java.io.File;
 import java.util.ArrayList;
 
-public class MainActivity extends BaseActivity<MainMvpView, MainPresenter> implements MainMvpView, View.OnClickListener {
-    Handler foregroundHandler;
-    Handler backgroundHandler;
-
-    HandlerThread handlerThread;
-
-    GridView imageGridView;
+public class MainActivity extends BaseActivity<MainMvpView, MainPresenter> implements MainMvpView, View.OnClickListener, AdapterView.OnItemClickListener {
+    GridView gv;
     ArrayList<File> list;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        list=imageReader( Environment.getExternalStorageDirectory() );
-        imageGridView =findViewById(R.id.imageGridView);
-        imageGridView.setAdapter( new GridAdapter());
-
-
-        imageGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                startActivity( new Intent(getApplicationContext(), ShowImageFullScreen.class).putExtra("img", list.get(i).toString() ) );
-            }
-        });
-
-
-    }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
     }
-    @Override
-    protected MainPresenter initPresenter() {
-        return new MainPresenter();
-    }
+
     @Override
     protected void startUI() {
-
-        foregroundHandler = new Handler(Looper.getMainLooper());
-        handlerThread = new HandlerThread("Background", Thread.MAX_PRIORITY);
-        handlerThread.start();
-
-        backgroundHandler = new Handler(handlerThread.getLooper());
+        list = presenter.imageReader(Environment.getExternalStorageDirectory() );
+        gv = findViewById(R.id.imageGridView);
+        gv.setAdapter(new GridAdapter());
+        gv.setOnItemClickListener(this);
     }
 
     @Override
@@ -73,109 +44,53 @@ public class MainActivity extends BaseActivity<MainMvpView, MainPresenter> imple
     }
 
     @Override
-    public void showMessage(final String msg) {
-        foregroundHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MainActivity.this, msg,Toast.LENGTH_LONG).show();
-            }
-        });
+    protected MainPresenter initPresenter() {
+        return new MainPresenter();
     }
+
+
     @Override
     public void onClick(View v) {
-      /* if(v.getId() == R.id.presenter_button){
-            presenter.showMessage();
-        }else {
-            foregroundThread();
-            //backgroundThread();
 
-        }
-*/
     }
 
-    /*****************Simple thread ****************/
-
-    private void foregroundThread(){
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Toast.makeText(MainActivity.this, "Button Click",Toast.LENGTH_LONG).show();
-            }
-        };
-
-        foregroundHandler.post(runnable);
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        startActivity(new Intent(getApplicationContext(), ShowImageFullScreen.class).putExtra("img", list.get(position)));
     }
 
+    @Override
+    public void showMessage(String msg) {
 
-    private void backgroundThread(){
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Toast.makeText(MainActivity.this, "Button Click",Toast.LENGTH_LONG).show();
-            }
-        };
-
-        backgroundHandler.post(runnable);
     }
 
     class GridAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return list.size();//returns number of items in grid
+            return list.size();
         }
 
         @Override
-        public Object getItem(int i) {
-            return list.get(i);//returns the i-th positioned data of list
+        public Object getItem(int position) {
+            return list.get(position);
         }
 
-        //this function is to use database
         @Override
-        public long getItemId(int i) {
+        public long getItemId(int position) {
             return 0;
         }
 
-        //it's the main task. It sets every image
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate( R.layout.single_grid, viewGroup, false ); //the custom grid_view, which i wanna show
-            //inflate is used to pick a XML file to java
-            //the object convertView holds everything of that layout. We can process/access the input section of that layout
+        public View getView(int position, View convertView, ViewGroup parent) {
 
-            ImageView iv= (ImageView) view.findViewById(R.id.imageView);
+            convertView = getLayoutInflater().inflate(R.layout.single_grid, parent, false);
+            ImageView iv = convertView.findViewById(R.id.imageView);
 
-            iv.setImageURI( Uri.parse( getItem(i).toString()) );
-            return view;
+            iv.setImageURI(Uri.parse(getItem(position).toString() ));
+
+
+            return convertView;
         }
     }
-    // reads all the existing files in root
-    ArrayList<File> imageReader(File root){
-
-        ArrayList<File> a = new ArrayList<>();
-        File[] files= root.listFiles();
-        for(int i=0; i<files.length; i++){
-            if(files[i].isDirectory()){
-                a.addAll(imageReader(files[i]));
-            }
-            else {
-                if( files[i].getName().endsWith(".jpg") ){
-                    a.add( files[i] );
-                }
-            }
-        }
-        return  a;// returns a File-typed array List, which reads all the jpj files in sdCARD
-    }
-
-
 }
--- app/src/main/java/com/w3/learn/main/MainPresenter.java
+++ app/src/main/java/com/w3/learn/main/MainPresenter.java
@@ -15,6 +15,9 @@ package com.w3.learn.main;
 import com.w3.learn.base.BasePresenter;
 import com.w3.learn.base.MvpView;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainPresenter extends BasePresenter<MainMvpView> {
 
     public void showMessage(){
@@ -24,5 +27,23 @@ public class MainPresenter extends BasePresenter<MainMvpView> {
                 getMvpView().showMessage("From Presenter");
             }
         }).start();
+    }
+
+    // reads all the existing files in root
+    public ArrayList<File> imageReader(File root){
+
+        ArrayList<File> a = new ArrayList<>();
+        File[] files= root.listFiles();
+        for(int i=0; i<files.length; i++){
+            if(files[i].isDirectory()){
+                a.addAll(imageReader(files[i]));
+            }
+            else {
+                if( files[i].getName().endsWith(".jpg") ){
+                    a.add( files[i] );
+                }
+            }
+        }
+        return  a;
     }
 }
